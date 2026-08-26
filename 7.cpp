fde2513cc0081585fba39de64f62a41df71ae8d5
Juan Robles <juroblesf@unal.edu.co>
2026-08-26 17:29:07 -0500

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,74 @@
+// Ejercicio. Haga un programa Dados dos arreglos sin repetidos, calcule el arreglo que resulta de la intersección entre los elementos de los otros dos arreglos. Este último debe ser definido del tamaño exacto.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *arr1; 
+    int *arr2;
+    int *arr3;
+    int size1, size2;
+
+    // Leer el tamaño del arreglo 1
+    scanf("%d", &size1);
+
+    // Asignación de memoria dinámica para el arreglo 1
+    arr1 = new int[size1]; 
+    if (arr1 == NULL) {
+        printf("Error al asignar memoria\n");
+        return 1;
+    }
+
+    // Leer los elementos del arreglo 1
+    for (int i = 0; i < size1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+    
+    
+    // Leer el tamaño del arreglo 2
+    scanf("%d", &size2);
+    
+    // Asignación de memoria dinámica para el arreglo 2
+    arr2 = new int[size2]; 
+    if (arr2 == NULL) {
+        printf("Error al asignar memoria\n");
+        return 1; 
+    }
+    
+    // Leer los elementos del arreglo 2
+    for (int i = 0; i < size2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+    
+    
+    
+    //Creamos un nuevo arreglo con la intersección
+    arr3 = new int[size1]; //Lo puse de tamaño size1 porque sí xd, la intersección nunca
+    									     //será de mayor alguno de los conjuntos
+    if (arr3 == NULL) {
+        printf("Error al asignar memoria\n");
+        return 1; 
+    }
+    
+    int index = 0;
+    for(int i = 0; i < size1; i++){
+    	for(int j = 0; j < size2; j++){
+    		if(arr1[i]==arr2[j]){
+    			arr3[index] = arr1[i];
+    			index ++;
+			}
+		}
+	}
+    
+    // Imprimir los elementos del arreglo
+    for (int i = 0; i < index; i++) {
+        printf("%d\n", arr3[i]);
+    }
+    
+    // Liberar la memoria dinámica asignada
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+
+    return 0;
+}
